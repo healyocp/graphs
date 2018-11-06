d64ec6367f3bb2db12285002a857c9ab4683637d
Portia Healy O'Connor <portiahealyoconnor@Portias-MacBook.local>
2018-11-06 15:27:16 +0000

    addition of funtions on data frame

--- r_projects.R
+++ r_projects.R
@@ -47,6 +47,50 @@ gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
 data <- fromJSON("https://api.github.com/users/healyocp")
 data$followers #number of followers i have
 data$public_repos #number of public repositories i have
+data$id
+data$url
+data$followers_url
+data$following_url
+data$name
+data$company
+data$following
+data$name
+data$name
+data$created_at
+data$updated_at
+data$bio
+data$email
+data$type
+data$site_admin
+data$public_gists
+data$public_repos
+data$hireable
+data$login
+data$blog
+data$location
+data$repos_url
+
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # specific details about followers must add to the url 
 followers <- fromJSON("https://api.github.com/users/healyocp/followers")
